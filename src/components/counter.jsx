33e73799befd94de8d5d6de427269acbf3bf8fed
Walkover <walkover@Walkovers-MacBook-Pro.local>
2021-05-29 21:05:54 +0530

    components : 11- binding event handlers : finished

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,12 +6,12 @@ class Counter extends Component {
         count: 0
     };
 
-    constructor() {
-        super();
-        this.handleIncrement = this.handleIncrement.bind(this);
-    }
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
 
-    handleIncrement() {
+    handleIncrement = () => {
         console.log('handle increment ', this);
     }
 
