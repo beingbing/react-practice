d9e2ebd052bf2b758b7b5a100b3e5a5eb421510e
Walkover <walkover@Walkovers-MacBook-Pro.local>
2021-05-29 20:58:47 +0530

    components : 10- handling events : finished

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,22 +3,31 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count: 0,
-        tags: []
+        count: 0
     };
 
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>Nothing to show!!</p>
-
-        return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>;
+    handleIncrement() {
+        console.log('handle increment');
     }
 
     render() { 
         return (
         <div>
-            {this.state.tags.length === 0  && 'Please create new tags'}
-            {this.renderTags()} </div>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">increment</button>
+        </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count ? "primary" : "warning";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count ? count : 'zero';
     }
 }
  
